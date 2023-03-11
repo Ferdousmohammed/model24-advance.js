e76fc15d6acad6a0da1f79da846b056714487952
Ferdousmohammed <ferdousmohammed29@gmail.com>
2023-03-11 19:34:38 +0600

    undefine vs null

---
+++ script.js
@@ -0,0 +1,33 @@
+const age = 4;
+
+if (age) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
+
+
+// undefine & null
+
+// 
+
+function add(num1, num2) {
+    var love = (num1 + num2);
+    // no return 
+    return love;
+}
+const result = add(10, 12);
+console.log(result)
+// console.log(pakhi);  
+
+
+// 3rd way of undefine
+function addy(nu, num) {
+    console.log(nu);
+}
+const res = addy(13);
+console.log(res);
+
+// output undefined
+
+//  Bandor type er developer
